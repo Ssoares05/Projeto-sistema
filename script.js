c5d7b9da823264edfbdc933d2f249e27a89a6898
Matheus Soares <ms5010858@gmail.com>
2026-06-04 23:52:17 -0300

    ATUALIZÇÃO
    Grande mudança no leyaut
    mudança de cores no body e titulo , com cores mais neutras e profissonal.
    sombras de textos nos input.
    e começo de ultilização de javascript com ALERT nos botoes

---
+++ script.js
@@ -0,0 +1,19 @@
+function filtrarVerificaçao()
+{
+    alert("Filtrar em Desenvolvimento");
+}
+
+function cadastrarVerificaçao()
+{
+    alert("Cadastrar em Desenvolvimento");
+}
+
+function limparVerificaçao()
+{
+    alert("Limpar em Desenvolvimento");
+}
+
+function anexarVerificaçao()
+{
+    alert("Anexar em Desenvolvimento");
+}
